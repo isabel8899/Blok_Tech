7567df234c15ddd5286f61fa6626ebd75683badd
isabel8899 <95106559+isabel8899@users.noreply.github.com>
2022-06-03 12:45:25 +0200

    camera aanvragen werkt

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const knop = document.querySelector('.toevoegen')
 
 knop.addEventListener('click', event => {
-  console.log('hoi')
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(mediaStream => {
+      document.querySelector('video').srcObject = mediaStream
+
+      const track = mediaStream.getVideoTracks()[0]
+      imageCapture = new ImageCapture(track)
+    })
+    .catch(error => ChromeSamples.log(error))
 })
+
+document.querySelector('#getUserMediaButton').addEventListener('click', knop)
+
+/* https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Image_Capture_API */
