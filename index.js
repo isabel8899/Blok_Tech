031a1f32d3d8c93a8878bf47a985b2b3870c951c
isabel8899 <95106559+isabel8899@users.noreply.github.com>
2022-06-03 13:09:45 +0200

    goede fonts-size

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ knop.addEventListener('click', event => {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(mediaStream => {
       document.querySelector('video').srcObject = mediaStream
-
-      const track = mediaStream.getVideoTracks()[0]
     })
     /* https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Image_Capture_API */
 })
