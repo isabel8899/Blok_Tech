92a144c532a99d0c3d934eaee0cb283201d85eef
isabel8899 <95106559+isabel8899@users.noreply.github.com>
2022-06-03 12:58:00 +0200

    camera kleiner

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ knop.addEventListener('click', event => {
       document.querySelector('video').srcObject = mediaStream
 
       const track = mediaStream.getVideoTracks()[0]
-      imageCapture = new ImageCapture(track)
     })
-    .catch(error => ChromeSamples.log(error))
+    /* https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Image_Capture_API */
 })
 
-document.querySelector('#getUserMediaButton').addEventListener('click', knop)
-
-/* https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Image_Capture_API */
+document.querySelector('.toevoegen').addEventListener('click', knop)
